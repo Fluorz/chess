245cc553f18c336fdfec44e64c4ff5e476c67b54
D1ADEV <D1ADEV@users.noreply.github.com>
2016-03-22 09:51:01 +0100

    Doc

--- Backend/RequestHandler.py
+++ Backend/RequestHandler.py
@@ -2,13 +2,27 @@ from SessionStorage import SessionStorage
 from RequestKind import RequestKind
 from Logger import Logger
 
-
+'''
+Classe qui gère les requètes
+'''
 class RequestHandler:
-
+    
+    '''
+    DONE
+    Initialisation
+    Args : Aucun
+    Return : Aucun
+    '''
     def __init__(self):
         Logger.log('Request handler init')
         self.SessionStorage = SessionStorage()
 
+    '''
+    DONE
+    Distribue les requètes vers des fonctions
+    Args : Requète
+    Return : Valeur renvoyée par la fonction 
+    '''
     def dispatchRequest(self, rq):
         Logger.log('dispatching request : ' + str(rq.requestKind))
         if rq.requestKind is not False:
@@ -23,7 +37,13 @@ class RequestHandler:
         else:
             return False
 
-    def handleNewGameRequest(self, rq):
+    '''
+    DONE
+    Fonction qui gère les requètes de création de nouvelle partie
+    Args : Aucun
+    Return : Valeur renvoyée par la SessionStorage.addNewSession()
+    '''
+    def handleNewGameRequest(self):
         res = self.SessionStorage.addNewSession()
         return res
 
