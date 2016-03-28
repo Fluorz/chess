d11d7cb8c4b9210d577a4eacd50200957a43ccce
D1ADEV <nicolasmanichon@yahoo.fr>
2016-03-28 17:19:53 +0200

    Joining and Creating games now works

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,49 +1,189 @@
 var isDragging = false;
 var oldPos = [-1, -1];
+var playerTurn = -1;
+var url = -1;
+var id = -1;
+var ready = false;
 
-var game = [[0, '.', 1, 1], [0, '.', 1, 2], [0, '.', 1, 3], [0, '.', 1, 4], [0, '.', 1, 5], [0, '.', 1, 6], [0, '.', 1, 7], [0, '.', 1, 8], [0, '.', 2, 1], [0, '.', 2, 2], [0, '.', 2, 3], [0, '.', 2, 4], [0, '.', 2, 5], [0, '.', 2, 6], [0, '.', 2, 7], [0, '.', 2, 8], [0, '.', 3, 1], [0, '.', 3, 2], [0, '.', 3, 3], [0, '.', 3, 4], [0, '.', 3, 5], [0, '.', 3, 6], [0, '.', 3, 7], [0, '.', 3, 8], [0, '.', 4, 1], [0, '.', 4, 2], [0, '.', 4, 3], [0, '.', 4, 4], [0, '.', 4, 5], [0, '.', 4, 6], [0, '.', 4, 7], [0, '.', 4, 8], [0, '.', 5, 1], [0, '.', 5, 2], [0, '.', 5, 3], [0, '.', 5, 4], [0, '.', 5, 5], [0, '.', 5, 6], [0, '.', 5, 7], [0, '.', 5, 8], [0, '.', 6, 1], [0, '.', 6, 2], [0, '.', 6, 3], [0, '.', 6, 4], [0, '.', 6, 5], [0, '.', 6, 6], [0, '.', 6, 7], [0, '.', 6, 8], [0, '.', 7, 1], [0, '.', 7, 2], [0, '.', 7, 3], [0, '.', 7, 4], [0, '.', 7, 5], [0, '.', 7, 6], [0, '.', 7, 7], [0, '.', 7, 8], [0, '.', 8, 1], [0, '.', 8, 2], [0, '.', 8, 3], [0, '.', 8, 4], [0, '.', 8, 5], [0, '.', 8, 6], [0, '.', 8, 7], [0, '.', 8, 8]];
-
-var html = '';
-html += ' <div id="chessTable"> <table id="e"> <thead> <tr> <th> </th> <th> 1</th> <th> 2 </th> <th> 3 </th> <th> 4 </th> <th> 5 </th> <th> 6 </th> <th> 7 </th> <th> 8 </th> </tr> </thead> <tbody> ';
-
-for (var y = 1; y < 9; y++) {
-    html += '<tr><td class="num">' + y + '</td>';
-    for (var x = 0; x < 9; x++) {
-        game.forEach(function (e) {
-            if (e[3] == y && e[2] == x) {
-                html += '<td onclick="processClick(' + x + ', ' + y + ');"><img src="assets/roi32x32noir.png"></td>';
-            }
-        })
-    }
-    html += '</tr>';
-}
-
-html += '</tbody></table></div>';
-$('#b').append(html);
-
-function processClick(x, y) {
-    var piece = 0;
-    if (isDragging == false) {
-        game.forEach(function (e) {
-            if (e[2] == x && e[3] == y) {
-                piece = e;
-            }
-        });
-        console.log(piece);
-        isDragging = true;
-        oldPos = [piece[2], piece[3]];
+var processClick = function(x, y){
+    if(ready === true){
+        if(isDragging === false){
+            oldPos = [x, y];
+            isDragging = true;
+        }
+        else{
+            sendMove(oldPos[0], oldPos[1], x, y);
+        }   
     }
     else{
-        sendMove(oldPos[0], oldPos[1], x, y);
+        alert('The game isn\'t ready. Please wait for another player to join.');
     }
-}
+};
 
-function sendMove(oldX, oldY, x, y){
-	var id = 12;
-	var url = 456;
-    console.log('oldx ' + oldX + ' oldy ' + oldY + ' x ' + x + ' y ' + y);
-	$.ajax('http://localhost/move/' + url.toString() + '/' + id.toString(), function(res){
-		console.log(res);
-	});
+// Proud of this one
+var getIndexFromPosition = function(x, y){
+    if(y == 1){
+        return x - 1;
+    }
+    else{
+        return ((((y - 1) * 8) + x) - 1);
+    }
+};
+
+var sendMove = function(oldX, oldY, x, y){
+    var array = {'oldX': oldX, 'oldY': oldY, 'x': x, 'y': y, 'id': id};
+    console.log(array);
+    $.post('http://localhost:8000/move/' + url + '/' + id, {'move': JSON.stringify(array)}, function(data){
+        console.log(data);
+    });
     isDragging = false;
-}
+};
+
+$(function(){
+    
+    var game = {};
+    
+    /*
+    ONLY FUNCTIONS THAT MATTER
+    */
+    
+    var createAndJoinGame = function(){
+        create(function(){
+            join(function(){
+                console.log('id : ' + id + ' url : ' + url);     
+                getGameState(function(data){
+                     refresh(data, function(){
+                         setInterval(function(){
+                             getGameState(function(temp){
+                                 refresh(temp); 
+                             });
+                         }, 10000);
+                     });
+                });
+            });
+        });
+    };
+    
+    var joinExistingGame = function(uniqueurl){
+        url = uniqueurl;
+        join(function(){
+            console.log('id : ' + id + ' url : ' + url);    
+            getGameState(function(data){
+                refresh(data, function(){
+                    setInterval(function(){
+                        getGameState(function(temp){
+                            refresh(temp)
+                        });
+                    }, 10000);
+                });
+            });
+        });
+    };
+    
+    $('#join').click(function(){
+        joinExistingGame($('#idInput').val());
+    });
+    
+    $('#create').click(function(){
+        createAndJoinGame();
+    });
+    
+    
+    /*
+    THESE FUNCTIONS ARE 'PRIVATE', AND SHOULDNT BE CALLED
+    */
+    
+    var create = function(cb){
+        $.ajax('http://localhost:8000/createnewgame').done(function(data){
+            if(data != 'False'){
+                var parsed = JSON.parse(data);
+                url = parsed.uniqueurl;
+                cb();
+            }
+            else{
+                console.log('wtf');
+            }
+        });
+    };
+    
+    var join = function(cb){
+        $.ajax('http://localhost:8000/joingame/' + url).done(function(data){
+            if(data != 'False'){
+                var parsed = JSON.parse(data);
+                id = parsed.id;
+                cb();
+            }
+            else{
+                console.log('wtf2');
+            }
+        });
+    };
+    
+    var render = function(){
+        var html = '';
+        html += ' <div id="chessTable"> <table id="e"> <thead> <tr> <th> </th> <th> 1</th> <th> 2 </th> <th> 3 </th> <th> 4 </th> <th> 5 </th> <th> 6 </th> <th> 7 </th> <th> 8 </th> </tr> </thead> <tbody> ';
+        
+        var x = 1;
+        var y = 1;
+        for(var i = 0; i < game.board.length; i++){
+            if(x > 8){
+                html += '</tr>';
+                x = 1;
+                y++;
+            }
+            if (x == 1){
+                html += '<tr><td class="num">' + y + '</td>';
+            }
+            //html += '<td onclick="processClick(' + x + ', ' + y + ');"><img src="assets/roi32x32noir.png"></td>';
+            var index = getIndexFromPosition(y, x);
+            if(game.board[index] != 'E'){
+                html += '<td onclick="processClick(' + x + ', ' + y + ');">' + game.board[index] + '</td>'; // (y, x) et pas (x, y). Pourquoi? Je ne sais pas. Mais ça march
+            }
+            else{
+                html += '<td onclick="processClick(' + x + ', ' + y + ');"></td>';
+            }
+            x++;
+        }
+        html += '</tbody></table></div>';
+        $('#b').append(html);
+    };
+    
+    var getGameState = function(cb){
+        console.log('getting game state');
+        $.ajax('http://localhost:8000/gameupdate/' + url).done(function(data){
+            cb(JSON.parse(data));
+        });
+    };
+    
+    var flushHtml = function(){
+        $("#b").empty();
+    };
+    
+    var refresh = function(newB, cb) { // cb is optional, not using it in the main setInterval
+        if(newB.board != game.board){
+            game = newB;
+            flushHtml();
+            render();
+            if(cb !== undefined){ // Only calling the cb if it is needed
+                cb();
+            }
+        }
+        else {
+            console.log('same');
+            if(cb !== undefined){ // Same 
+                cb();    
+            }
+        }
+        ready = game.ready;
+    };
+    
+    var stopRefreshing = function(){
+        clearInterval(refreshInterval);
+    };
+});
+
+
+
+
+
+
